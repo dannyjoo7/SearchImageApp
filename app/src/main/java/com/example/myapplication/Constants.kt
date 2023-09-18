e133185daf377d3c255285eebc273cf5681b40bc
찬영짱 <58664438+dannyjoo7@users.noreply.github.com>
2023-09-18 23:06:40 +0900

    특정 파일 삭제

--- app/src/main/java/com/example/myapplication/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-class Constants {
-    companion object {
-        const val BASE_URL = "https://dapi.kakao.com"
-
-        const val AUTH_HEADER = "KakaoAK cf6022ad29355367fda52a5bfcbb5947"
-    }
-}
