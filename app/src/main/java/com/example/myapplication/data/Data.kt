4ad342f168f2140e0c30ee6dad1266ea2dbc4f94
dannyjoo7 <danny_joo@naver.com>
2023-09-22 16:54:34 +0900

    FIX: 깃 오류 해결

--- app/src/main/java/com/example/myapplication/data/Data.kt
+++ app/src/main/java/com/example/myapplication/data/Data.kt
@@ -12,14 +12,7 @@ object Data {
             mutableListOf()
         }
 
-    private val favoriteData: MutableList<Item> =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mutableListOf(
-                Item(title="(No Title)", display_sitename="티스토리", image_url="https://blog.kakaocdn.net/dn/deu5VO/btsu0t56Rwg/2eFeBJxsDbVBomId0R9sk1/img.jpg", datetime=Date(), isFavorite=true)
-            )
-        } else {
-            mutableListOf()
-        }
+    private val favoriteData: MutableList<Item> = mutableListOf()
 
     fun getSearchData(): MutableList<Item> {
         return searchData
